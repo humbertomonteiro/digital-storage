72e1918d7a1868b57d728113ee87f6baa24f58fb
Humberto Monteiro <humbertoaraujox@icloud.com>
2024-11-26 10:03:31 -0300

    add estruturas de pastas

---
+++ src/pages/Categories/index.jsx
@@ -0,0 +1,9 @@
+const Categories = () => {
+  return (
+    <div>
+      <h1>Em breve...</h1>
+    </div>
+  );
+};
+
+export default Categories;
---
+++ src/pages/MyOrders/index.jsx
@@ -0,0 +1,9 @@
+const MyOrders = () => {
+  return (
+    <div>
+      <h1>Em breve...</h1>
+    </div>
+  );
+};
+
+export default MyOrders;
