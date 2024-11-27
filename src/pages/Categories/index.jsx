fce6b3f38774a6434e9ce589a5d0425b7cc1681e
GregoriusBlack <luanjg7@gmail.com>
2024-11-26 21:53:37 -0300

    adicionando imagens no assets

--- src/pages/Categories/index.jsx
+++
@@ -1,9 +0,0 @@
-const Categories = () => {
-  return (
-    <div>
-      <h1>Em breve...</h1>
-    </div>
-  );
-};
-
-export default Categories;
--- src/pages/MyOrders/index.jsx
+++
@@ -1,9 +0,0 @@
-const MyOrders = () => {
-  return (
-    <div>
-      <h1>Em breve...</h1>
-    </div>
-  );
-};
-
-export default MyOrders;
