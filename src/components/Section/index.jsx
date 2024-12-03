b8f79160b6e4e2aa59f7062d7291a42df1da5f71
Humberto Monteiro <humbertoaraujox@icloud.com>
2024-12-02 21:36:42 -0300

    att

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,14 +1,13 @@
-import React from "react";
-
-const Section = ({props}) => {
-
-    return(
-        <div>
-            <h4 style={{float:props.titleAlign}}>{props.href}</h4>
-            <a href={props.link.text}>{props.text}</a>
-            {props.children}
-        </div>
-    );
+const Section = ({ title, titleAlign, link, children }) => {
+  return (
+    <section>
+      <div style={{ textAlign: { titleAlign } }}>
+        <h2>{title}</h2>
+        {link && <a href={link.href}>{link.text}</a>}
+      </div>
+      <div>{children}</div>
+    </section>
+  );
 };
 
 export default Section;
